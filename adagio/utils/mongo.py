cc4d116ccf9c5bb423528a813b53a380672b0476
thoriuchi0531 <nekopuni@gmail.com>
2018-04-14 06:00:17 +0100

    Added rtype

--- adagio/utils/mongo.py
+++ adagio/utils/mongo.py
@@ -6,7 +6,9 @@ arctic_store = Arctic(AdagioConfig.arctic_host)
 
 
 def get_library(library_name):
-    """ Return arctic library. Library is initialised if not exists """
+    """ Return arctic library. Library is initialised if not exists
+    :rtype: arctic.store.version_store.VersionStore
+    """
     libraries = arctic_store.list_libraries()
     if library_name not in libraries:
         arctic_store.initialize_library(library_name)
